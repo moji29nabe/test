54a72e86fb2ebed45a7d55415f43e42f07285bf4
moji29nabe <moji29nabe@users.noreply.github.com>
2014-01-24 23:18:19 +0900

    C++ で sqlite3 に接続した

--- TestCppProj/Debug/a/subdir.mk
+++ TestCppProj/Debug/a/subdir.mk
@@ -4,34 +4,40 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../a/TestCgi.cpp 
+../a/AcessSqlite.cpp \
+../a/TestCgi.cpp \
+../a/Thread.cpp 
 
 C_SRCS += \
 ../a/Addition.c 
 
 OBJS += \
+./a/AcessSqlite.o \
 ./a/Addition.o \
-./a/TestCgi.o 
+./a/TestCgi.o \
+./a/Thread.o 
 
 C_DEPS += \
 ./a/Addition.d 
 
 CPP_DEPS += \
-./a/TestCgi.d 
+./a/AcessSqlite.d \
+./a/TestCgi.d \
+./a/Thread.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-a/%.o: ../a/%.c
-	@echo 'Building file: $<'
-	@echo 'Invoking: Cross GCC Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
 a/%.o: ../a/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
 	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+a/%.o: ../a/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
---
+++ TestCppProj/a/AcessSqlite.cpp
@@ -0,0 +1,25 @@
+/*
+ * AcessSqlite.cpp
+ * $sudo apt-get install libsqlite3-dev が必要だった
+ */
+
+#include <stdio.h>
+#include <cstdlib>
+#include <sqlite3.h>
+
+int main(int argc, char* argv[])
+{
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+
+   rc = sqlite3_open("test.db", &db);
+
+   if( rc ){
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      exit(0);
+   }else{
+      fprintf(stderr, "Opened database successfully\n");
+   }
+   sqlite3_close(db);
+}
